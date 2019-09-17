28b9614d3c6c5b751831e94e4a5dbb6c3c06394a
Alexander <lxgolovin@gmail.com>
2019-09-17 23:05:04 +0300

    Initial commit

---
+++ installation-of-i3/500-install-applications-v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sh applications/install-atom-v*.sh
+
+sh applications/install-google-chrome-v*.sh
+
+sh applications/install-spotify-v*.sh
+
+sh applications/install-vivaldi-snapshot-v*.sh
+
+#sh applications/install-zsh-v*.sh
+
+
+echo "#########################################################"
+echo "Applications have been  installed"
+echo "#########################################################"
+sleep 1
